cac2794f8b1de623a33ec866b6793466dc0720a5
Steve Hocking <steve.hocking@ivendi.com>
2019-07-08 13:07:43 +0100

    Initial setup for retry app

---
+++ PollyRetry.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PollyRetry.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
