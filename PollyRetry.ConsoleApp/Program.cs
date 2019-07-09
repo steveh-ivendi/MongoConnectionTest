17a6f7e57c2e67856c6511a20868ab6cbf5be30c
Steve Hocking <steve.hocking@ivendi.com>
2019-07-09 10:03:06 +0100

    Basic code to get a collection

--- PollyRetry.ConsoleApp/Program.cs
+++ PollyRetry.ConsoleApp/Program.cs
@@ -1,71 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Polly;
+using MongoDB.Driver;
 using Serilog;
 
 namespace PollyRetry.ConsoleApp
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console().CreateLogger();
 
-            var timeoutList = new List<TimeSpan>{
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(2),
-                TimeSpan.FromSeconds(3),
-                TimeSpan.FromSeconds(5),
-                TimeSpan.FromSeconds(8),
-                TimeSpan.FromSeconds(13),
-                TimeSpan.FromSeconds(21),
-                TimeSpan.FromSeconds(34)
-            };
-
-            Log.Logger.Information("Starting...");
-
-            var count = 0;
-
-            var answer = await Policy.Handle<Exception>().WaitAndRetryAsync(timeoutList, (e, ts, i, c) => {
-                if (e is TimeoutException)
-                {
-                    Log.Logger.Error($"Timed out! Timespan: {ts}; Retry: {i}; Context: {c}");
-                }
-                else
-                {
-                    Log.Logger.Error(e, $"Timespan: {ts}; Retry: {i}; Context: {c}");
-                }
-            })
-            .ExecuteAndCaptureAsync(async () => {
-                if(count++ < 10)
-                {
-                    if (count % 2 == 0)
-                    {
-                        throw new ArgumentNullException("myParam");
-                    }
-
-                    if (count % 5 == 0)
-                    {
-                        throw new ArithmeticException("something awful")
-                    }
-
-                    throw new TimeoutException("No, you're the victim");
-                }
-
-                await Task.Delay(TimeSpan.FromSeconds(30));
-                return 42;
-            });
-
-            if(answer.Outcome == OutcomeType.Successful)
+            try
             {
-                Log.Logger.Information($"Received answer: {answer.Result}");
+                Log.Logger.Debug("Starting to get collection");
+
+                var mongoUrl = MongoUrl.Create("mongodb://127.0.0.1:27017/quoteware-bc-local");
+                var mongoClient = new MongoClient(mongoUrl);
+                var mongoDatabase = mongoClient.GetDatabase(mongoUrl.DatabaseName);
+                var collection = mongoDatabase.GetCollection<ProductRateReadModel>("ProductRateReadModel");
+
+                Log.Logger.Information(collection.EstimatedDocumentCount() < 1 ? "Collection is empty" : $"Collection has an estimated {collection.EstimatedDocumentCount()} documents");
             }
-            else
+            catch(Exception e)
             {
-                Log.Logger.Fatal($"No answer returned: {answer.FaultType}");
+                Log.Logger.Error(e, "Failed to get collection");
             }
         }
+    }
+
+    public class ProductRateReadModel
+    {
+        public Guid Id {get; set;}
     }
 }
